e0922a5fa9b2d16543509b3929c92133166e474b
Beautus <gumedesduduzo@gmail.com>
2020-05-23 04:19:52 +0200

    added tailwind configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
+// import "./registerServiceWorker"; # Eventually this has to be enabled but with a functioning config
 import router from "./router";
 import store from "./store";
+import "./assets/tailwind.css";
 
 createApp(App as object)
   .use(router)
