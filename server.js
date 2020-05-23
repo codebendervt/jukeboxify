8eeb1352b2d398a907a462bba8fda9d78d157907
Beautus <gumedesduduzo@gmail.com>
2020-05-23 03:44:13 +0200

    server init

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello ther");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require("path");
+const express = require("express");
+const config = require("./api/config");
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+config(app);
+
+const publicPath = resolve(__dirname, "./dist");
+const staticConf = { maxAge: "1y", etag: false };
+app.use(express.static(publicPath, staticConf));
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`> Ready On Port ${PORT}`);
+});
